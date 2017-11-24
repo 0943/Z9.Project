4613d9b0dc7fdc7e2400c2cbf38aeb294a858474
Harry Stone <Harry943@outlook.com>
2017-11-24 13:57:23 +0800

    Update ViewModel_C#7.x.cs

--- test/ViewModel_C#7.x.cs
+++ test/ViewModel_C#7.x.cs
@@ -1,6 +1,6 @@
-﻿/*
- * This demo's code syntax use for C# 7.x, can be use in Visual Studio 2017.
- */
+/*
+* This demo's code syntax use for C# 7.x, can be use in Visual Studio 2017.
+*/
 
 using System.Windows;
 using System.Windows.Input;
@@ -9,74 +9,74 @@ using Z9.Mvvm.Command;
 
 namespace WpfTemp
 {
-	class MainViewModel : NotificationObject
-	{
-		#region Property
+    class MainViewModel : NotificationObject
+    {
+        #region Property
 
-		// Original
-		int prop1;
-		public int Prop1
-		{
-			get => prop1; 
-			set { prop1 = value; OnPropertyChanged(nameof(Prop1)); }
-		}
+        // Original
+        int prop1;
+        public int Prop1
+        {
+            get => prop1;
+            set { prop1 = value; OnPropertyChanged(nameof(Prop1)); }
+        }
 
-		// Complex
-		int prop2;
-		public int Prop2
-		{
-			get => prop2;
-			set { prop2 = value; OnPropertyChanged(); }	// OnPropertyChanged method can automatically get the property name.
-		}
+        // Complex
+        int prop2;
+        public int Prop2
+        {
+            get => prop2;
+            set { prop2 = value; OnPropertyChanged(); } // OnPropertyChanged method can automatically get the property name.
+        }
 
-		// Simple
-		string prop3;
-		public string Prop3
-		{
-			get => prop3; 
-			set => Set(ref prop3, value);	// Set method can automatically get the property name, too.
-		}
+        // Simple
+        string prop3;
+        public string Prop3
+        {
+            get => prop3;
+            set => Set(ref prop3, value);   // Set method can automatically get the property name, too.
+        }
 
-		// Most simple way (no private member anymore).
-		public double Prop4
-		{
-			get => GetProperty<double>();
-			set => SetProperty(value);
-		}
+        // Most simple way (no private member anymore).
+        public double Prop4
+        {
+            get => GetProperty<double>();
+            set => SetProperty(value);
+        }
 
-		#endregion
+        #endregion
 
-		#region Command
+        #region Command
 
-		// Normal command
-		public ICommand Change => new RelayCommand(() =>
-		{
-			Prop1 = 943;
-			Prop2 = 0x3af;
-		});
+        // Normal command
+        public ICommand Change => new RelayCommand(() =>
+        {
+            Prop1 = 943;
+            Prop2 = 0x3af;
+        });
 
-		// Command width parameter
-		public ICommand Check => new RelayCommand<string>(
-			str => MessageBox.Show(str));
+        // Command width parameter
+        public ICommand Check => new RelayCommand<string>(
+            str => MessageBox.Show(str));
 
-		// Command width parameter and can execution method
-		public ICommand Test => new RelayCommand<string>(
-			str => { }, 
-			str => true
-		);
-			
-		#endregion
+        // Command width parameter and can execution method
+        public ICommand Test => new RelayCommand<string>(
+            str => { },
+            str => true
+        );
 
-		protected override void OnInitializeInDesignMode()
-			=> Prop3 = $"This is in design mode, you can see this sentence in your xaml designer when a control binding on this property.";
+        #endregion
 
-		protected override void OnInitializeInRuntime()
-			=> Prop3 = $"This is in run-time mode, you can see this sentence in running window when a control binding on this property.";
+        protected override void OnInitializeInDesignMode()
+            => Prop3 = $"This is in design mode, you can see this sentence in your xaml designer when a control binding on this property.";
 
-		// No need use constructor any more
-		//public MainViewModel()
-		//{
+        protected override void OnInitializeInRuntime()
+            => Prop3 = $"This is in run-time mode, you can see this sentence in running window when a control binding on this property.";
 
-		//}
-	}
+        // No need use constructor any more
+        //public MainViewModel()
+        //{
+
+        //}
+    }
 }
